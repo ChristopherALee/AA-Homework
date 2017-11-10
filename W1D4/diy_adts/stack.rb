c12e1952206e0bfff84e3fbf158fd5be7ae02e6d
ChristopherALee <christopheradamlee@gmail.com>
2017-11-09 20:19:00 -0500

    Completed Exercise 1 - Stack

--- W1D4/diy_adts/stack.rb
+++ W1D4/diy_adts/stack.rb
@@ -1,17 +1,23 @@
 class Stack
+  attr_accesor :ivar
+
   def initialize
     # create ivar to store stack here!
+    @ivar = []
   end
 
   def add(el)
     # adds an element to the stack
+    ivar.push(el)
   end
 
   def remove
     # removes one element from the stack
+    ivar.pop
   end
 
   def show
     # return a copy of the stack
+    ivar.dup
   end
 end
